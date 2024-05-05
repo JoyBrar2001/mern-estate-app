b7cad38debb3744e2ab60ce9b9a6964f592b6254
Joy Brar <joybrar2001@gmail.com>
2024-05-05 16:31:30 +0530

    Added Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SingUp from './pages/SingUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SingUp />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SingUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
