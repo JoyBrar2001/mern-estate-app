aee352bd38cf606c9207c0696322263482ec27ca
Joy Brar <joybrar2001@gmail.com>
2024-05-07 06:07:12 +0530

    add redux persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -36,7 +36,7 @@ const SignIn = () => {
         return;
       }
 
-      dispatch(signInSuccess());
+      dispatch(signInSuccess(data));
       navigate('/');
   
     } catch (error) {
@@ -70,7 +70,7 @@ const SignIn = () => {
 
       <div className='flex gap-2 mt-5'>
         <p>Don't have an account ?</p>
-        <Link to={"/sign-in"}>
+        <Link to={"/sign-up"}>
           <span className='text-blue-700'>Sign up</span>
         </Link>
       </div>
