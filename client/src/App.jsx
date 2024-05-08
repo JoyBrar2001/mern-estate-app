d1f958388f0bae3ee1587319c76c667fc06f416a
Joy Brar <joybrar2001@gmail.com>
2024-05-08 20:22:03 +0530

    Protected Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SingUp from './pages/SingUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
@@ -16,7 +17,9 @@ const App = () => {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SingUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
