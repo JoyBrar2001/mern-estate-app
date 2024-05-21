75afa1d212cf3e449ccd154727eedc383fcd398b
Joy Brar <joybrar2001@gmail.com>
2024-05-22 04:02:47 +0530

    Added Update Listing API Route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const verifyToken = require('../utils/verifyUser');
-const { createListing, deleteListing } = require('../controllers/listing.controller');
+const { createListing, deleteListing, updateListing } = require('../controllers/listing.controller');
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing);
 
 module.exports = router;
